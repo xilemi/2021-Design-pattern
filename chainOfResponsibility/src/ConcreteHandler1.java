bcca5579519493d059858e436d19f084aade4007
xilemi <31377899+xilemi@users.noreply.github.com>
2021-06-18 23:09:32 +0800

    职责链模式

--- chainOfResponsibility/src/ConcreteHandler1.java
+++ chainOfResponsibility/src/ConcreteHandler1.java
@@ -1,5 +1,4 @@
 public class ConcreteHandler1 extends Handler{
-    @Override
     public void HandleRequest(int request) {
         if(request>=0&&request<10){
             System.out.println(this.getClass().getName()+"处理请求");
