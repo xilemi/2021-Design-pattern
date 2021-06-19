9173971912ffbe5bd9da3cc868d764b4d4457a8b
xile <1223162589@qq.com>
2021-06-19 14:15:57 +0800

    解释器模式

---
+++ InterpreterPattern/src/InterpreterPatternDemo.java
@@ -0,0 +1,24 @@
+public class InterpreterPatternDemo {
+    //规则：Robert 和 John 是男性
+    public static Expression getMaleExpression(){
+        Expression robert = (Expression) new TerminalExpression("Robert");
+        Expression john = (Expression) new TerminalExpression("John");
+        return new OrExpression(robert, john);
+    }
+
+    //规则：Julie 是一个已婚的女性
+    public static Expression getMarriedWomanExpression(){
+        Expression julie = (Expression) new TerminalExpression("Julie");
+        Expression married = (Expression) new TerminalExpression("Married");
+        return new AndExpression(julie, married);
+    }
+
+    public static void main(String[] args) {
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+
+        System.out.println("John is male? " + isMale.interpret("John"));
+        System.out.println("Julie is a married women? "
+                + isMarriedWoman.interpret("Married Julie"));
+    }
+}
